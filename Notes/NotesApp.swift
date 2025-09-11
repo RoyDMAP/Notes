34d20fe1f8ad149a541913c3e96d1a9834b10642
RoyDMAP <roy.dimapilis@yahoo.com>
2025-09-10 19:55:51 -0700

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,20 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Roy Dimapilis on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
